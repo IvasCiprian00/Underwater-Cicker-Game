32e096a087ee51c04961124b07c727ad6557ea2b
Ciprian Ivas <ciprian@chirpy.digital>
2023-11-22 18:03:33 +0200

    Add shark boss

--- Assets/Scripts/SharkMovement.cs
+++ Assets/Scripts/SharkMovement.cs
@@ -9,23 +9,70 @@ public class SharkMovement : MonoBehaviour
     enum State
     {
         prowling,
-        attacking
+        attacking,
+        retreating
     }
 
     private State state;
 
+    private float timer;
+
     private void Awake()
     {
-        fish.SetSpeed(Random.Range(2f, 3f));
+        fish.SetSpeed(7f);
 
         state = State.attacking;
     }
 
     private void Update()
     {
-        if(state == State.attacking)
+        fish.Movement();
+
+        if(state == State.retreating && transform.position.y < -4f) 
         {
-            fish.Movement();
+            state = State.prowling;
+            fish.speed = 1.3f;
+            timer = 0f;
+
+            transform.rotation = (Random.Range(1, 3) == 1) ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
+        }
+
+        if(state == State.prowling)
+        {
+            if(transform.position.x < -2.5f)
+            {
+                transform.rotation = Quaternion.identity;
+            }
+
+            if(transform.position.x > 2.5f)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+
+            timer += Time.deltaTime;
+        }
+
+        if(timer > 5f && state == State.prowling)
+        {
+            state = State.attacking;
+
+            float angle = Mathf.Atan(7 / Mathf.Abs(transform.position.x)) * 180 / Mathf.PI;
+            int flipImage = transform.position.x > 0 ? 180 : 0;
+
+            fish.speed = 7f;
+
+            transform.rotation = Quaternion.Euler(0, flipImage, angle);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Boat")
+        {
+            fish.gameManager.DamageShip(fish.damage);
+            transform.rotation = Quaternion.Euler(0, 0, -90);
+            fish.SetSpeed(4f);
+            state = State.retreating;
         }
     }
 }
