a6692e2736fe3c21fcddadf5a2f55505f83c9fc2
Ciprian Ivas <ciprian@chirpy.digital>
2023-11-23 21:48:01 +0200

    Add fish attack

--- Assets/Scripts/BasicFishMovement.cs
+++ Assets/Scripts/BasicFishMovement.cs
@@ -1,27 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class BasicFishMovement : MonoBehaviour
 {
     public Fish fish;
 
+    enum State
+    {
+        moving,
+        charging,
+        attacking
+    }
+
+    private State state;
+
     private void Awake()
     {
         fish.SetSpeed(Random.Range(0.9f, 1.1f));
+        
+        state = State.moving;
     }
 
     private void Update()
     {
         fish.Movement();
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Boat")
         {
-            Destroy(gameObject);
+            state = State.charging;
+
+            fish.speed = -0.5f;
+
             fish.gameManager.DamageShip(fish.damage);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Boat")
+        {
+            state = State.attacking;
+
+            StartCoroutine(ChargeAttack());
+        }
+    }
+
+    IEnumerator ChargeAttack()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        fish.speed = 1f;
     }
 }
